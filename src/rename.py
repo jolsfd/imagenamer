a75b82c55281efb9e1b68cccce132e46cc67059a
jolsfd <amoe19be@t-online.de>
2021-03-12 16:09:50 +0100

    define Setting object

--- src/rename.py
+++ src/rename.py
@@ -1,1 +1,10 @@
 import os, json
+
+class Settings:
+    def __init__(path_to_settings):
+        self.path_to_settings = path_to_settings
+        self.settings_template = {
+            'safe_string':'IMG',
+            'file_ext':['.jpg'],
+            'space_letter':'_'
+        }
