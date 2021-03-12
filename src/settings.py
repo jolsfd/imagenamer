8cd1cf4bf829357f77795eb54c3bd5d319a3fb97
jolsfd <amoe19be@t-online.de>
2021-03-12 16:19:29 +0100

    bugfix

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,7 @@
 import os, json
 
 class Settings:
-    def __init__(path_to_settings):
+    def __init__(self, path_to_settings):
         self.path_to_settings = path_to_settings
         self.settings_template = {
             'safe_string':'IMG',
