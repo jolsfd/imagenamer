fbc3578156f5191692e32b00fb79077926b15c15
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-03-12 16:17:46 +0100

    add load_settings definition

--- src/rename.py
+++ src/rename.py
@@ -8,3 +8,17 @@ class Settings:
             'file_ext':['.jpg'],
             'space_letter':'_'
         }
+
+    def load_settings(self):
+        try:
+            json_file = open(self.path_to_settings ,'r')
+
+            settings = json.load(json_file)
+
+            json_file.close()
+
+            return settings
+        except FileNotFoundError:
+            self.save_settings(self.settings_template)
+
+            return self.settings_template
