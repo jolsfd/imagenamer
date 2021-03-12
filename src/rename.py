9604466949f6d1e342cf5dfccd4be55a96e46de6
jolsfd <amoe19be@t-online.de>
2021-03-12 16:58:22 +0100

    add new_filename function

--- src/rename.py
+++ src/rename.py
@@ -4,3 +4,16 @@ from exif import Image
 class Rename:
     def __init__(self, settings):
         self.settings = settings
+
+    def new_filename(self, path_to_file):
+        with open(path_to_file, 'rb') as image_file:
+            image_exif = Image(path_to_file)
+
+        if image_exif.has_exif:
+            space_letter = self.settings['space_letter']
+            datetime = image_exif.datetime_original().replace(':','').replace(' ',space_letter)
+            model = image_exif.model().replace(' ','')
+
+            new_filename = self.settings['safe_string'] + space_letter + datetime + space_letter + model
+            
+            return new_filename
