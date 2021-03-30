7e4c75e8ff9fcf333db658e0161829c69792ba4d
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-03-30 12:26:49 +0200

    format code

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from os import path
 from src.menu import Menu
 import os
 
+
 def run():
-    path_to_settings = os.path.join(os.path.dirname(os.path.abspath(__file__))) + '/settings/settings.json'
+    path_to_settings = (
+        os.path.join(os.path.dirname(os.path.abspath(__file__)))
+        + "/settings/settings.json"
+    )
     menu = Menu(path_to_settings)
 
     if not menu.error:
@@ -14,23 +18,24 @@ def run():
     else:
         running = False
 
-    print('Version 1.0')    
+    print("Version 1.0")
 
     while running:
-        print(F'\nType "rename", "help", "settings", "quit"')
-        user_input = input('>>> ')
+        print(f'\nType "rename", "help", "settings", "quit"')
+        user_input = input(">>> ")
 
-        if user_input == 'rename':
+        if user_input == "rename":
             menu.rename()
 
-        elif user_input == 'settings':
+        elif user_input == "settings":
             menu.new_settings()
 
-        elif user_input == 'help':
+        elif user_input == "help":
             menu.help()
 
-        elif user_input == 'quit':
+        elif user_input == "quit":
             running = False
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
