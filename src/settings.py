2337a5490e4b6359c8f7afca9a3ec88de2225364
jolsfd <76818674+jolsfd@users.noreply.github.com>
2021-03-30 12:26:00 +0200

    format code

--- src/settings.py
+++ src/settings.py
@@ -1,33 +1,34 @@
 import os, json
 from colorama import Fore, Back, Style
 
+
 class Settings:
     def __init__(self, path_to_settings):
         self.path_to_settings = path_to_settings
         self.settings_template = {
-            'safe_string':'IMG',
-            'image_ext':['.jpg','.JPG'],
-            'space_letter':'_',
-            'raw_rename':True,
-            'raw_ext':['.raw',".cr2",".dng"]
+            "safe_string": "IMG",
+            "image_ext": [".jpg", ".JPG"],
+            "space_letter": "_",
+            "raw_rename": True,
+            "raw_ext": [".raw", ".cr2", ".dng"],
         }
 
     def save_settings(self, new_settings):
         try:
-            json_file = open(self.path_to_settings, 'w')
+            json_file = open(self.path_to_settings, "w")
 
             json.dump(new_settings, json_file)
 
             json_file.close()
 
-            print(Fore.GREEN + F'Saved Changes into json file.' + Fore.RESET)
+            print(Fore.GREEN + f"Saved Changes into json file." + Fore.RESET)
 
         except:
-            print(Fore.RED + F'Settings could not be saved' + Fore.RESET)
+            print(Fore.RED + f"Settings could not be saved" + Fore.RESET)
 
     def load_settings(self):
         try:
-            json_file = open(self.path_to_settings ,'r')
+            json_file = open(self.path_to_settings, "r")
 
             settings = json.load(json_file)
 
@@ -43,25 +44,29 @@ class Settings:
         error = False
 
         try:
-            if type(settings['safe_string']) != type(str()):
+            if type(settings["safe_string"]) != type(str()):
                 error = True
 
-            if type(settings['image_ext']) != type(list()):
+            if type(settings["image_ext"]) != type(list()):
                 error = True
 
-            if type(settings['space_letter']) != type(str()):
+            if type(settings["space_letter"]) != type(str()):
                 error = True
 
-            if type(settings['raw_rename']) != type(bool()):
+            if type(settings["raw_rename"]) != type(bool()):
                 error = True
 
-            if type(settings['raw_ext']) != type(list()):
+            if type(settings["raw_ext"]) != type(list()):
                 error = True
 
         except AttributeError:
             error = True
 
         if error:
-            print(Fore.RED + F'Error in settings. Please visit https://github.com/jolsfd/imagenamer' + Fore.RESET)
+            print(
+                Fore.RED
+                + f"Error in settings. Please visit https://github.com/jolsfd/imagenamer"
+                + Fore.RESET
+            )
 
         return error
